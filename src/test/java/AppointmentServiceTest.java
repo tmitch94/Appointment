7851f3cf5d3976bdd6a4daae0fe95e5ec4a23fc8
Tacia Mitchell <Tacia. Mitchell@icloud.com>
2024-02-07 21:22:53 -0500

    created test case for appointment service

---
+++ src/test/java/AppointmentServiceTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppointmentServiceTest {
+
+
+    @Test
+    void addAppointment() {
+    }
+
+    @Test
+    void deleteAppointment() {
+    }
+}
