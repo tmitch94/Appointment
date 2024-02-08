9f63a3836bec0da2a6d8629f7041c9cd4c3aca29
Tacia Mitchell <Tacia. Mitchell@icloud.com>
2024-02-07 21:19:25 -0500

    made changes to main and AppointmentService

--- src/main/java/AppointmentService.java
+++ src/main/java/AppointmentService.java
@@ -8,30 +8,26 @@ public class AppointmentService {
 
     HashMap<String,Appointment> appointmentHashMap = new HashMap<>();
 
-
-
-    public void addAppointment(){
-        String pattern = "MM/dd/yy";
-        SimpleDateFormat format = new SimpleDateFormat(pattern);
-
-        try {
-            Date date = format.parse("02/05/24");
-            Appointment appointment = new Appointment("1234", date,"");
+    public void addAppointment(Appointment appointment){
             appointmentHashMap.put(appointment.getAppointmentID(),appointment);
-            for (Appointment appointments: appointmentHashMap.values()){
-                System.out.println(appointments.getAppointmentID() + appointments.getDate() + appointments.getDescription());
-            }
-        }catch (ParseException e){
-            System.out.println("Could not read date");
-        }
     }
 
 
 
     public void deleteAppointment(String key){
         if (appointmentHashMap.containsKey(key)){
+            appointmentHashMap.remove(key);
+            System.out.println( key + "Removed");
+            displayAppointments();
+        }else {
+            System.out.println("Not removed");
+        }
+    }
+    private void displayAppointments(){
+        for (Appointment appointments: appointmentHashMap.values()) {
+            System.out.println(appointments.getAppointmentID() + appointments.getDate() + appointments.getDescription());
 
         }
-
     }
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,21 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+
+        String pattern = "MM/dd/yyyy";
+        SimpleDateFormat format = new SimpleDateFormat(pattern);
+        Date date = format.parse("02/05/24");
         AppointmentService appointmentService = new AppointmentService();
-        appointmentService.addAppointment();
+        Appointment appointment = new Appointment("1234 ",date," Test");
+        Appointment appointment2 = new Appointment("4567 ",date," Tests");
+        Appointment appointment3 = new Appointment("8945 ",date," Testing");
+        appointmentService.addAppointment(appointment);
+        appointmentService.addAppointment(appointment2);
+        appointmentService.addAppointment(appointment3);
+        appointmentService.deleteAppointment(appointment.getAppointmentID());
+        appointmentService.deleteAppointment(appointment2.getAppointmentID());
     }
 }
